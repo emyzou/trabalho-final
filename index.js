0a79c2d7f69d83921166a1da825dc31bc0cc6733
Emilly Agatha <135267138+emyzou@users.noreply.github.com>
2023-09-18 13:44:10 -0300

    index.js

--- index.js
+++ index.js
@@ -1,52 +1,92 @@
 const tarefas = [];
 
-function criarTarefas() {
-    
-    const statusDaTarefa = {
-        titulo: prompt('tituloDaSuaTarefa'),
-        descricao: prompt('descriçao da sua tarefa'),
-        status: prompt('a tarefa esta concluida?')
 
-    };
-    tarefas.push();
-    console.log(statusDaTarefa)
+function criarTarefa() {
+  const titulo = prompt("Digite o título da tarefa:");
+  const descricao = prompt("Digite a descrição da tarefa:");
+  const status = "pendente";
+
+  const novaTarefa = {
+    titulo,
+    descricao,
+    status
+  };
+
+  tarefas.push(novaTarefa);
+ console.log('A tarefa foi criada');
+}
+
+
+
+function visualizarTarefas() {
+  if (tarefas.length === 0) {
+   console.log('nenhuma tarefa foi cadastrada');
+  } else {
+    console.log("Lista de tarefas:");
+    tarefas.forEach((tarefa, indicie) => {
+    console.log(`Tarefa ${indicie + 1}:`);
+   console.log(`Título: ${tarefa.titulo}`);
+      console.log(`Descrição: ${tarefa.descricao}`);
+      console.log(`Status: ${tarefa.status}`);
+  console.log("");
+    });
   }
-  
-  function listarTarefas() {
-    console.log("tarefas;");
+}
+
+
+function editarTarefa() {
+  const indice = prompt("Digite o índice da tarefa que deseja editar:") - 1;
+
+  if ( indice < 0 || indice >= tarefas.length) {
+    console.log("Índice inválido");
+  } else {
+    const novoTitulo = prompt("Digite o novo título da tarefa:");
+    const novaDescricao = prompt("Digite a nova descrição da tarefa:");
+
+    tarefas[indice].titulo = novoTitulo;
+    tarefas[indice].descricao = novaDescricao;
+    console.log("Tarefa editada com sucesso");
   }
-  
-  function editarTarefas() {
-    console.log('');
+}
+
+
+function apagarTarefa() {
+  const indice = prompt("Digite o índice da tarefa que deseja apagar:") - 1;
+
+  if (indice || indice < 0 || indice >= tarefas.length) {
+    console.log("Índice inválido.");
+  } else {
+    tarefas.splice(indice, 1);
+    console.log("Tarefa apagada com sucesso");
   }
 
-  function apagarTarefas() {
-    console.log("Você escolheu a Opção 3.");
-  }
 
+}
+
+
+let escolha;
+do {
+  escolha = prompt("Escolha uma opção: Numero1: Criar Tarefa,  Numero2: Visualizar Tarefas,  Numero3: Editar Tarefa,  Numero4: Apagar Tarefa,  Numero5: Sair");
+  switch (escolha) {
+    case "1":
+      criarTarefa();
+    break;
+    case "2":
+      visualizarTarefas();
   
-  function exibirMenu() {
-    console.log();
-    console.log("1. Opção 1");
-    console.log("2. Opção 2");
-    console.log("3. Opção 3");
-    
+      break;
+    case "3":
+      editarTarefa();
+  
+      break;
+    case "4":
+      apagarTarefa();
+   break;
+    case "5":
+      console.log("Saindo do programa");
+   break;
+    default:
+      console.log("Opção inválida. Tente novamente");
   }
-  
-  exibirMenu();
-  
-  const escolha = prompt('Escolha uma opção: Número 1- criar tarefa, Número 2- listar tarefa, Número 3- editar, Numero 4- apagar ');
-  
-  if (escolha === "1") {
-    criarTarefas();
-  } else if (escolha === "2") {
-    listarTarefas();
-  } else if (escolha === "3") {
-    editarTarefas();
-  } else if (escolha === "4") {
-    apagarTarefas();
-    
-  } else{
-    console.log("Opção inválida.");
-  }
+} while (escolha !== "5");
   
